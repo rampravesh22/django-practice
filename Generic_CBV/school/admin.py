1e833e6bf1f2e9c47d6d5f16be2cfa2196371a16
rpc <rpc7863@gmail.com>
2022-07-10 10:22:55 +0530

    new

--- Generic_CBV/school/admin.py
+++ Generic_CBV/school/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from school.models import *
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','name','roll','course']
--- Generic_CBV/school/views.py
+++ Generic_CBV/school/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from school.models import Student
 # Create your views here.
+class StudentListView(ListView):
+    model = Student
+    

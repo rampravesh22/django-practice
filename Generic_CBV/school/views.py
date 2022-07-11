e103b416f681c5cad9799f56ca6ab212f0186b6a
rpc <rpc7863@gmail.com>
2022-07-11 07:51:54 +0530

    a

--- Generic_CBV/school/views.py
+++ Generic_CBV/school/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from school.models import Student
 # Create your views here.
+# ListView Example
 class StudentListView(ListView):
     model = Student
-    
+

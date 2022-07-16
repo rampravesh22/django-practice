3225a7cbf775c2c4a91fc04313e18063687c9f7e
rpc <rpc7863@gmail.com>
2022-07-16 11:25:20 +0530

    new

--- Generic_CBV/school/views.py
+++ Generic_CBV/school/views.py
@@ -1,5 +1,6 @@
 from ctypes import Union
 from typing import Optional
+from django import forms
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -26,4 +27,8 @@ class CreateStudentView(CreateView):
     fields = ['name', 'course', 'roll']
     template_name: str = 'school/createView/student_form.html'
     success_url: Optional[str] = '/create/'
-
+    
+    def get_form(self):
+        form = super().get_form()
+        form.fields['name'].widget = forms.TextInput(attrs={'class':"my-class"})
+        return form

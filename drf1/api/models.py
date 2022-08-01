5945bbd6553f9a9f5c9af78a93496b28f8cdee1e
rpc <rpc7863@gmail.com>
2022-08-01 22:46:33 +0530

    just commit

--- drf1/api/models.py
+++ drf1/api/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    roll = models.ImageField()
+    roll = models.IntegerField()
     city = models.CharField(max_length=50)
--- drf1/api/views.py
+++ drf1/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Student
+from .serializers import StudentSerializer
 # Create your views here.
+
+
+def student_detail(request):
+    stu = Student.objects.get(id=1)
+    serializer = StudentSerializer(stu)

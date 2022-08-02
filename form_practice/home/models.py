ff79c19a479cbf6c20eb8371a9a3954678836279
rpc <rpc7863@gmail.com>
2022-08-02 19:09:34 +0530

    just commit

--- form_practice/home/models.py
+++ form_practice/home/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
-
-
-class Student(models.Model):
-    gender_choice = (
-        ("Male", 'Male'),
-        ("Female", "Female"),
-        ("Other", "Other")
-    )
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    gender = models.CharField(choices=gender_choice)
+# class Student(models.Model):
+#     gender_choice = (
+#         ("Male", 'Male'),
+#         ("Female", "Female"),
+#         ("Other", "Other")
+#     )
+#     name = models.CharField(max_length=100)
+#     age = models.IntegerField()
+#     gender = models.CharField(choices=gender_choice)

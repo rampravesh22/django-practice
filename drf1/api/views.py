9358cfbb8e6a3121452471cdbac0e41fee5f17e6
rpc <rpc7863@gmail.com>
2022-08-02 12:42:31 +0530

    just commit

--- drf1/api/views.py
+++ drf1/api/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 
 def student_detail(request):
-    stu = Student.objects.get(id=2)
-    serializer = StudentSerializer(stu)
-    jason_data = JSONRenderer().render(serializer.data)
-    print(jason_data)
-    return HttpResponse(jason_data, content_type="aplication/json")
+    stu = Student.objects.all()
+    serializer = StudentSerializer(stu, many=True)
+    return JsonResponse(serializer.data, safe=False)

eb2e77a374caff3508a4bef2a5816edd899b59fc
rpc <rpc7863@gmail.com>
2022-08-02 20:37:01 +0530

    just commit

--- formp/core/forms.py
+++ formp/core/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class StudentRegister(forms.Form):
     name = forms.CharField(
-        required=True, label_suffix="::", initial="Enter your name")
-    age = forms.IntegerField()
+        required=True, label_suffix=":",widget=forms.TextInput(attrs={"placeholder":"Enter your name"}))
+    age = forms.IntegerField(max_value=100,min_value=18)
     gender = forms.CharField(max_length=100)
     

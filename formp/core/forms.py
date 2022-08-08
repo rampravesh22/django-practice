0733f1d13a9ced1490057e8e68489f04a69d23b9
rpc <rpc7863@gmail.com>
2022-08-08 10:20:44 +0530

    just commit

--- formp/core/forms.py
+++ formp/core/forms.py
@@ -5,6 +5,6 @@ class StudentRegister(forms.Form):
     name = forms.CharField(
         initial="", required=True, label_suffix=":", widget=forms.TextInput(attrs={"class": "form-control","atuocomplete":"off"}))
     age = forms.IntegerField(max_value=100, min_value=18, widget=forms.NumberInput(
-        attrs={"class": "form-control"}))
+        attrs={"class": "form-control","atuocomplete":"off"}))
     gender = forms.ChoiceField(choices=SELECT_GENDER, widget=forms.Select(
         attrs={"class": "form-select w-25"}))

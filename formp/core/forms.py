3b5a2a17e7dd613c5afc4471bbd9bfdd780be5fc
rpc <rpc7863@gmail.com>
2022-08-08 10:07:44 +0530

    just commit

--- formp/core/forms.py
+++ formp/core/forms.py
@@ -3,7 +3,7 @@ from .choices import SELECT_GENDER
 
 class StudentRegister(forms.Form):
     name = forms.CharField(
-        initial="", required=True, label_suffix=":", widget=forms.TextInput(attrs={"class": "form-control"}))
+        initial="", required=True, label_suffix=":", widget=forms.TextInput(attrs={"class": "form-control","atuocomplete":"off"}))
     age = forms.IntegerField(max_value=100, min_value=18, widget=forms.NumberInput(
         attrs={"class": "form-control"}))
     gender = forms.ChoiceField(choices=SELECT_GENDER, widget=forms.Select(
--- formp/core/models.py
+++ formp/core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from core.choices import SELECT_GENDER
 # Create your models here.
+import random
 
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(choices=SELECT_GENDER,max_length=100)
+    
+

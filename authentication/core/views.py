31fe3fc556e38dd187eac17298ea76661d179a98
rpc <rpc7863@gmail.com>
2022-08-10 21:53:56 +0530

    just commit

--- authentication/core/views.py
+++ authentication/core/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # from core.forms import Register
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
     if request.method=="POST":
         form = UserCreationForm(request.POST)
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
         if form.is_valid():
             form.save()
+            print("********************************************************")
+            return redirect("/")
+        else:
+            print("####################################################")
     form=UserCreationForm()
     context={
         "form":form

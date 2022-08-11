334e6f3c617405db1babd020e9d5a94c61514015
rpc <rpc7863@gmail.com>
2022-08-11 10:28:16 +0530

    just commit

--- auth/core/views.py
+++ auth/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
@@ -6,6 +6,9 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            return redirect(request.META.get('HTTP_REFERER'))
     else:
         form = UserCreationForm()
     context={

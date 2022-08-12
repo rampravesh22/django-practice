2f7c5d4bf9791a630903b3f1a19182554d7ca8c3
rpc <rpc7863@gmail.com>
2022-08-12 21:23:56 +0530

    just commit

--- sessions/student/views.py
+++ sessions/student/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from datetime import datetime,timedelta
 # Create your views here.
 def set_session(request):
     request.session['name'] = "Rampravesh Chaudhari"
+    request.session['age'] = 23
     return render(request, "student/setsession.html")
 
 
 def get_session(request):
-    name=request.session['name']
-    return render(request, "student/getsession.html",{'name':name})
+    name=request.session.get('name')
+    age=request.session.get('age')
+    print(request.session.keys())
+    print(request.session.values())
+    print(request.session.items())
+    return render(request, "student/getsession.html",{'name':name,'age':age})
 
 def del_session(request):
-    if 'name' in request.session:
-        del request.session['name'] 
+    request.session.flush()
     return render(request, "student/delsession.html")

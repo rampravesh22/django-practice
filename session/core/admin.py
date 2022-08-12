9c085ca432505010a30f7c5da7f131a9ebb9f38d
rpc <rpc7863@gmail.com>
2022-08-12 18:17:38 +0530

    just commit

--- session/core/admin.py
+++ session/core/admin.py
@@ -1,3 +1,11 @@
+from typing import Sequence
 from django.contrib import admin
-
+from core.models import Student
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id",'name', 'email', 'age', 'subjects')
+    list_editable = ('name', 'email', 'age', 'subjects')
+    # list_per_page: int = 2
+    search_fields = ("name",)
+    # list_filter = ('name',)
--- session/session/settings.py
+++ session/session/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
+X_FRAME_OPTIONS = "SAMEORIGIN"
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

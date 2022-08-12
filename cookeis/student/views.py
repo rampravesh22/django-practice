4bdf0097a8b572f716f1594765a8818ca119360d
rpc <rpc7863@gmail.com>
2022-08-12 12:41:02 +0530

    just commit

--- cookeis/student/views.py
+++ cookeis/student/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def set_cookies(request):
+    response = render(request, "student/setcookie.html")
+    response.set_cookie("name","Rampravesh")
+    return response
+
+def get_cookies(request):
+    print()
+    return render(request, "student/get_cookie.html")
+
+def del_cookies(request):
+    context={}
+    return render(request, "student/del_cookie.html", context)

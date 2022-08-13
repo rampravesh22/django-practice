c37f8e11eeac2622b4c8ab33c9117cc636563834
rpc <rpc7863@gmail.com>
2022-08-13 19:22:22 +0530

    just commit

--- page_counter/core/views.py
+++ page_counter/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    ct = request.session.get("count",0)
+    new_count= ct+1
+    request.session['count'] = new_count
+    context={
+        'c':new_count,
+    }
+    return render(request, "core/home.html", context)
+    

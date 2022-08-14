fd64dc7de86937d546daa44ee0db4dda308c6e11
rpc <rpc7863@gmail.com>
2022-08-14 12:55:22 +0530

    just commit

--- auth_system/auth_system/urls.py
+++ auth_system/auth_system/urls.py
@@ -1,4 +1,4 @@
-"""auth_system URL Configuration
+"""login_logout URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.home,name="home"),
+    path("register/", views.register, name="register"),
+    path('login/',views.user_login,name="login"),
+    path('profile/',views.profile,name="profile"),
+    path('logout/',views.log_out,name="logout"),
+    path('changewith/',views.change_pass_with_old_pass,name="changewith"),
+    path('changewithout/',views.change_pass_without_old_pass,name="changewithout"),
+    path("updateprofile/", views.update_profile, name="updateprofile"),
+    path("session-expired/", views.session_expired, name="session-expired")
 ]

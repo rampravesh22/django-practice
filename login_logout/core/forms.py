c73b472df992442e47225d8b54b9aa09bd5f5699
rpc <rpc7863@gmail.com>
2022-08-14 12:27:50 +0530

    just commit

--- login_logout/core/forms.py
+++ login_logout/core/forms.py
@@ -1,28 +1,35 @@
-from django.contrib.auth.forms import UserCreationForm,UserChangeForm,AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
 
+
 class SignUp(UserCreationForm):
-    password2 = forms.CharField(max_length=200, required=True,label="Password Confirmation",widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=200, required=True,
+                                label="Password Confirmation", widget=forms.PasswordInput())
     # email = forms.EmailField(required=True,error_messages={"required":"Hello there"})
+
     class Meta:
         model = User
         # fields = ["username"]
-        fields = ["username","first_name","last_name","email"]
-        
-        
+        fields = ["username", "first_name", "last_name", "email"]
+
+
 class UpdateUserForm(UserChangeForm):
     password = None
+
     class Meta:
         model = User
-        fields=["username","first_name","last_name",'email',"date_joined","email"]
-        
+        fields = ["username", "first_name", "last_name",
+                  'email', "date_joined", "email"]
+
+
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Email or Username",widget=forms.TextInput(attrs={"class":"form-control",'autocomplete':"off"}))
-    def __init__(self,*args ,**kwargs):
-        super().__init__( *args, **kwargs)
+    username = forms.CharField(label="Email or Username", widget=forms.TextInput(
+        attrs={"class": "form-control", 'autocomplete': "off"}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.fields['password'].widget.attrs.update({
-            'class':"form-control",
+            'class': "form-control",
             'autocomplete': 'current-password'
         })
-        
